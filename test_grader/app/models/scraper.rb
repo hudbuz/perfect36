2d0375182853ac155cc8f9e05204c340530d128b
hudbuz <hzb2@cornell.edu>
2016-07-07 18:09:20 -0400

    Multiple tests saved. The MC tests appear to be working fine with scraper. Looks like scraper could be adjusted to add multiple other tests with LOVI encryption. Work on User show pages and proper pdf display, in additon to validation and we should be done for now.

--- test_grader/app/models/scraper.rb
+++ test_grader/app/models/scraper.rb
@@ -20,19 +20,19 @@ class Scraper
       if !a.first.last.include?('fc0')
       if a.children.text.to_i > 0
       r = Answer.new(answer_key_id: key.id)
-      if a.first.last.include?('x2') || a.first.last.include?('x4')
+      if a.first.last.include?('x2') || a.first.last.include?('x4') || a.first.last.include?('x1')
         r.section = key.sections.where(title: 'english').first
         r.question = a.children.text.to_i
         r.save
-      elsif a.first.last.include?('x6') || a.first.last.include?('x8')
+      elsif a.first.last.include?('x6') || a.first.last.include?('x8') || a.first.last.include?('xe')
         r.section = key.sections.where(title: 'math').first
         r.question = a.children.text.to_i
         r.save
-      elsif a.first.last.include?('xa')
+      elsif a.first.last.include?('xa') || a.first.last.include?('xf')
       r.section = key.sections.where(title: 'reading').first
         r.question = a.children.text.to_i
         r.save
-      elsif a.first.last.include?('xc')
+      elsif a.first.last.include?('xc') || a.first.last.include?('x10')
         r.section = key.sections.where(title: 'science').first
         r.question = a.children.text.to_i
         r.save
