0bd2e797ab5d8dfa1f7f7e8d4a06c8f97476242c
hudbuz <hzb2@cornell.edu>
2016-12-14 16:56:01 -0500

    d3 graph exists trying to add radio now

--- test_grader/app/serializers/test_user_serializer.rb
+++ test_grader/app/serializers/test_user_serializer.rb
@@ -1,5 +1,5 @@
 class TestUserSerializer < ActiveModel::Serializer
-  attributes :id, :english_score, :reading_score, :math_score, :science_score, :total_score, :answer_key_id
+  attributes :id, :english_score, :reading_score, :math_score, :science_score, :total_score, :answer_key_id, :created_at
   has_one :test
   has_one :user
 end
